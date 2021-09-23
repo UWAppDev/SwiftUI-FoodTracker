346dea2395dd52f17d22f9833337368f1201a924
Xinyi Xiang <xinyixiang1129@gmail.com>
2021-09-22 20:58:03 -0700

    Set up for iOS 15

---
+++ Shared/FoodTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodTrackerApp.swift
+//  Shared
+//
+//  Created by Vicky Xiang on 9/22/21.
+//
+
+import SwiftUI
+
+@main
+struct FoodTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
